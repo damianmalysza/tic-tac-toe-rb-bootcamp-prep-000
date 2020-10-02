a8f033d702cb877afa43429bfc4ab4678b2a05e2
Damian Malysza <damian.malysza@gmail.com>
2020-10-02 02:25:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,3 +65,8 @@ def current_player(board)
     return "O"
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    if board[combo[0]] == "X" && board[combo[1] == "X" && board[combo[2]] == "X"
+end
