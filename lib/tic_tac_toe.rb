4d7ca7c5d381b8495462466e11bc8b2f6bbb89a0
Damian Malysza <damian.malysza@gmail.com>
2020-10-02 02:33:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,9 +68,8 @@ end
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
-    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
-      return true
-    elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+    if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X") ||
+      board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
       return true
     else
       return false
